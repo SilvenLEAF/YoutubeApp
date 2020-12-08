461a6ccb7f77d903afee2d3dd54b219f6437fab8
SilvenLEAF <SilvenLEAF@gmail.com>
2020-12-08 08:59:14 +0530

    Created Guardian Middlewares (for enhancing security)

--- routes/authRoutes/userRoute.ts
+++ routes/authRoutes/userRoute.ts
@@ -3,6 +3,16 @@ import * as userController from '../../controllers/userController';
 
 
 
+
+// -------------------middlewares-------------------
+import isLoggedin from '../../middlewares/isLoggedin';
+import DeleteAccount from '../../middlewares/DeleteAccount';
+import UpdateAccount from '../../middlewares/UpdateAccount';
+
+
+
+
+
 const router = Router();
 
 
@@ -10,16 +20,22 @@ const router = Router();
 
 
 
-
-
-router.get('/all', userController.get_all_users);
-router.delete('/', userController.delete_account);
-router.put('/', userController.update_account);
-
-router.put('/changeEmail', userController.change_email);
+// -------------------------GET LOGGED IN USER
+router.get('/', isLoggedin, (req, res, next)=>{
+  res.json({ user: req.user });
+});
 
 
 
+
+
+
+
+router.get('/all', isLoggedin, userController.get_all_users);
+router.delete('/', isLoggedin, DeleteAccount, userController.delete_account);
+router.put('/', isLoggedin, UpdateAccount, userController.update_account);
+
+router.put('/changeEmail', isLoggedin, UpdateAccount, userController.change_email);
 
 
 
