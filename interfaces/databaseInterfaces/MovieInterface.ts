adf89b68f48e195f4da58b30399ad86c14bcc0f4
SilvenLEAF <SilvenLEAF@gmail.com>
2020-12-28 08:16:10 +0530

    watchlist interface

--- interfaces/databaseInterfaces/MovieInterface.ts
+++ interfaces/databaseInterfaces/MovieInterface.ts
@@ -3,84 +3,22 @@ import mongoose, { Document } from 'mongoose'
 
 
 
-
-
-
-
-
-
-
-interface genreInterface {
-  "id": number,
-  "name": string
-}
-
-
-
-interface productionCompaniesInterface {
-  "id": number,
-  "logo_path"?: string,
-  "name": string,
-  "origin_country"?: string
-}
-
-
-
-interface productionCountriesInterface {
-  "iso_3166_1": string,
-  "name": string
-}
-
-
 /* ------------------------------------------
 .            MAIN INTERFACE
 ------------------------------------------ */
 export default interface MovieInterface extends Document {
+  "movieId": string,
+  
 
-  adult?: boolean,
-  backdrop_path?: string,
-  "belongs_to_collection"?: any,
-  budget?: number,
-  genres?: genreInterface[],
-
-
-
-  homepage?: string,
-  movieId: number,
-  "imdb_id"?: string | number,
-  "original_language"?: string,
-  "original_title"?: string,
-
-
-
-  overview?: string,
-  popularity?: number,
   "poster_path"?: string,
-  "production_companies"?: productionCompaniesInterface[],
-  "production_countries"?: productionCountriesInterface[],
-
-
-
-
-
-
   "release_date"?: string,
-  revenue?: number,
-  runtime?: number,
-  "spoken_languages"?: [],
-  "status"?: string,
-
-
-
-
-
   "tagline"?: string,
   "title"?: string,
-  "video"?: boolean,
   "vote_average"?: number,
-  "vote_count"?: number,
 
 
+  "liked": boolean,
+  
 }
 
 
